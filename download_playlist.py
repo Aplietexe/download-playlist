5c31da98765282172503025abb59532570307321
Pietro Palombini <pietropalo@icloud.com>
2024-08-21 22:23:07 -0300

    stuff

--- download_playlist.py
+++ download_playlist.py
@@ -2,7 +2,7 @@ from argparse import ArgumentParser
 from os import wait
 from subprocess import DEVNULL, Popen
 
-from pytube import Playlist
+from pytubefix import Playlist
 
 arg_parser = ArgumentParser()
 arg_parser.add_argument(
@@ -16,16 +16,23 @@ arg_parser.add_argument(
     default=5,
     type=int,
 )
+arg_parser.add_argument(
+    "--output",
+    help="Output directory",
+    default=".",
+    type=str,
+)
 args = arg_parser.parse_args()
 url = args.url
 concurrency = args.concurrency
+output = args.output
 
 
 playlist = Playlist(url)
 processes = set()
 counter = 0
 for video in playlist.video_urls:
-    processes.add(Popen(["pytube", video], stdout=DEVNULL))
+    processes.add(Popen(["pytubefix", video], stdout=DEVNULL))
     while len(processes) >= concurrency:
         wait()
         finished = {p for p in processes if p.poll() is not None}
@@ -33,3 +40,8 @@ for video in playlist.video_urls:
             counter += len(finished)
             print(f"Finished {counter}/{len(playlist.video_urls)}")
             processes.difference_update(finished)
+
+for p in processes:
+    p.wait()
+    counter += 1
+    print(f"Finished {counter}/{len(playlist.video_urls)}")
